844ae384387eabb7ef6f548a6fb8db2578ff4a17
Nasratie <nasratie@MacBook-Pro-de-Nasratie.local>
2023-12-19 12:58:48 +0100

    fonction add, get, list, remove

--- dictionnaire/main.go
+++ dictionnaire/main.go
@@ -8,7 +8,32 @@ import (
 
 func main() {
 
+dictionnaire := make(map[string]string)
+
+AjouterMot(dictionnaire, "python", "un langage d eprogrammation leger")
+AjouterMot(dictionnaire, "canvas", "un logiciel de creation graphique")
+AjouterMot(dictionnaire, "go", "un langage de programmation")
+
+
 }
 
+func add(dictionnaire map[string]string, mot, definition string) {
+	dictionnaire[mot] = definition
+}
 
+func get(dictionnaire map[string]string, mot string) string {
+	definition := dictionnaire[mot]
+	return definition
+}
 
+func remove(dictionnaire map[string]string, mot string) {
+	delete(dictionnaire, mot)
+}
+
+func liste(dictionnaire map[string]string) []string {
+	var resultat []string
+	for mot, definition := range dictionnaire {
+		resultat = append(resultat, fmt.Sprintf("%s: %s", mot, definition))
+	}
+	return resultat
+}
