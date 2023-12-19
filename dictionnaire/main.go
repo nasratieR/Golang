97a1cce0fa2188a0547354a3a537528482ef9e15
Nasratie <nasratie@MacBook-Pro-de-Nasratie.local>
2023-12-19 17:07:41 +0100

    fonction add, get, list, remove

--- dictionnaire/main.go
+++ dictionnaire/main.go
@@ -2,38 +2,51 @@ package main
 
 import (
 	"fmt"
-	"maps"
+	"sort"
 )
 
+type Dictionnaire map[string]string
+
+func (d Dictionnaire) Add(word, definition string) {
+	d[word] = definition
+}
+
+func (d Dictionnaire) Get(word string) string {
+	return d[word]
+}
+
+func (d Dictionnaire) Remove(word string) {
+	delete(d, word)
+}
+
+func (d Dictionnaire) List() []string {
+	var result []string
+	for word, definition := range d {
+		result = append(result, fmt.Sprintf("%s: %s", word, definition))
+	}
+
+	sort.Strings(result)
+	return result
+}
 
 func main() {
+	dictionnaire := make(Dictionnaire)
 
-dictionnaire := make(map[string]string)
+	dictionnaire.Add("python", "un lan gage de programmation leger")
+	dictionnaire.Add("canvas", "un logiciel de crétion graphique")
+	dictionnaire.Add("go", "un langage de programmation")
 
-Add(dictionnaire, "python", "un langage d eprogrammation leger")
-Add(dictionnaire, "canvas", "un logiciel de creation graphique")
-Add(dictionnaire, "go", "un langage de programmation")
+	wordToLookup := "go"
+	definition := dictionnaire.Get(wordToLookup)
+	fmt.Printf("Définition de %s: %s\n", wordToLookup, definition)
 
+	wordToRemove := "canvas"
+	fmt.Printf("suppression %s du dictionnnaire.\n", wordToRemove)
+	dictionnaire.Remove(wordToRemove)
 
-}
-
-func add(dictionnaire map[string]string, mot, definition string) {
-	dictionnaire[mot] = definition
-}
-
-func get(dictionnaire map[string]string, mot string) string {
-	definition := dictionnaire[mot]
-	return definition
-}
-
-func remove(dictionnaire map[string]string, mot string) {
-	delete(dictionnaire, mot)
-}
-
-func liste(dictionnaire map[string]string) []string {
-	var resultat []string
-	for mot, definition := range dictionnaire {
-		resultat = append(resultat, fmt.Sprintf("%s: %s", mot, definition))
+	fmt.Println("liste triée des mots et de leurs définitions")
+	wordList := dictionnaire.List()
+	for _, entry := range wordList {
+		fmt.Println(entry)
 	}
-	return resultat
 }
